ae53d2b7169cbdeaa5aef7ab31bd9b2921b9f769
Jade Armstrong <jadea@bbd.co.za>
2023-03-23 14:04:17 +0200

    Removed unused imports

--- demo/src/main/java/game/EnemyManager.java
+++ demo/src/main/java/game/EnemyManager.java
@@ -3,10 +3,6 @@ package game;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.HashMap;;
 
 public class EnemyManager {
     private static String[] enemyIntros = {" has appeared!", " waddled out of the wood work", " superman lands infront of you! BADASS", " wants to lick your elbow"};
